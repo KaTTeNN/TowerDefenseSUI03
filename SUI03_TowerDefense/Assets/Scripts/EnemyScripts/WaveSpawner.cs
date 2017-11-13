5c62f1a2a0488c5fb2dec2ebdb860640fdb90c5f
TobenTheGreat <33621588+TobenTheGreat@users.noreply.github.com>
2017-11-13 14:56:38 +0100

    Tobias - Waves
    Jobbar just nu på waves, snart färdigt

---
+++ SUI03_TowerDefense/Assets/Scripts/EnemyScripts/WaveSpawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+	public enum GameState {Spawning, Waiting, Counting};
+
+	[System.Serializable]
+	public class Wave
+	{
+		public string name;
+		public Transform enemy;
+		public int count;
+		public float rate;
+	}
+
+	public Wave[] waves;
+	int nextWave = 0; 
+
+	public float timeBetweenWaves = 10f;
+	public float waveCountdown;
+
+	private GameState state = GameState.Counting;
+
+	void Start () 
+	{
+		waveCountdown = timeBetweenWaves;
+	}
+	
+
+	void Update () 
+	{
+		if (waveCountdown <= 0) 
+		{
+			if (state != GameState.Spawning) 
+			{
+				StartCoroutine (SpawnWave(Wave[nextWave]));
+			}
+		} 
+		else 
+		{
+			waveCountdown -= Time.deltaTime;
+		}
+	}
+
+	IEnumerator SpawnWave(Wave wave)
+	{
+		state = GameState.Spawning;
+		for (int i = 0; i < wave.count; i++) 
+		{
+			EnemySpawning (wave.enemy);
+			yield return new WaitForSeconds (1f/wave.rate);
+		}
+		state = GameState.Waiting;
+		yield break;
+	}
+
+	public void EnemySpawning(Transform enemy)
+	{				
+		enemy = Instantiate (enemyPrefab) as GameObject;
+		enemy.transform.position = new Vector3 (71, 1, 25);
+	}
+}
